a6dee6894501ed47ed0cafbb4c5dbcd5dce93f70
knutux <knutux@gmail.com>
2018-03-17 23:31:27 +0200

    small changes

--- classes/PageRouter.php
+++ classes/PageRouter.php
@@ -6,30 +6,12 @@ class PageRouter
         {
         switch ($name)
             {
-            case 'lvl':
-                return "LevelList";
             case 'api':
                 return "ApiHandler";
-            case 'app':
-                return "ApplicationList";
-            case 'ver':
-                return "VersionList";
-            case 'cat':
-                return "CategoryList";
-            case 'create':
-                return "CheckersPuzzleCreatorPage";
-            case 'mgmt':
-                return "DataManagementPage";
             case 'aso':
                 return "ASOToolsPage";
             case 'asoapp':
                 return "ASOUnitPage";
-            case 'stats':
-                return "Statistics";
-            case 'ch-a':
-                return "ChartboostManager";
-            case 'ch-a-old':
-                return "ChartboostAnalytics";
             default:
                 return false;
                 
